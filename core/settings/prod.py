9320a94cd067739e5359726aed5b69c329d837ea
hone5t <hone5t@hone5t.com>
2020-04-12 21:13:07 -0500

    intial fork from django-docker

---
+++ core/settings/prod.py
@@ -0,0 +1,27 @@
+from .base import *
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG')
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", " ").split(" ")
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),       
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
+    }
+}
+CACHES = {
+    'default': {
+        'BACKEND': os.environ.get('CACHE'),
+        'LOCATION': os.environ.get('REDIS_SERVER'),
+    },
+}
