096170cbe11b43fdb1906c7864e4c0eb35d943d4
hone5t <hone5t@hone5t.com>
2020-04-13 01:51:47 -0500

    use formview instead of templateview for contact us

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import ContactUsView
 
 urlpatterns = [
     path('about-us/', TemplateView.as_view(template_name='main/about_us.html'), name='about_us'),
-    path('contact-us/', TemplateView.as_view(template_name='main/contact_us.html'), name='contact_us'),
+    path('contact-us/', ContactUsView.as_view(), name='contact_us'),
     path('', TemplateView.as_view(template_name='main/home.html'), name='home'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from . import forms
 
 # Create your views here.
 class ContactUsView(FormView):
-    template_name = "main/contact_form.html"
+    template_name = "main/contact_us.html"
     form_class = forms.ContactForm
     success_url = "/"
     def form_valid(self, form):
